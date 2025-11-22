a3a7604e51c5ecc9954aa162612b580c6414441d
jonasbarzee <jonascbarzee@gmail.com>
2025-11-21 21:17:49 -0700

    Inital Commit.

---
+++ src/pages/Home.jsx
@@ -0,0 +1,18 @@
+export const Home = () => {
+    return (
+    <div className="min-h-screen">
+        <h1>Hello world</h1>
+        {/* Theme Toggle */}
+
+
+        {/* Background Effects */}
+
+        {/* Navbar */}
+
+        {/* Main Content */}
+
+
+        {/* Footer */}
+    </div>
+    );
+};
